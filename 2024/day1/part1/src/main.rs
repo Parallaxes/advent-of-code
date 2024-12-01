8e3a36c0ec10bee842cf41347e61d1c94b9806b2
Parallaxes <lucasmasterson@gmail.com>
2024-12-01 10:08:22 -0800

    2024 Day 1 Complete

--- 2024/day1/part1/src/main.rs
+++ 2024/day1/part1/src/main.rs
@@ -1,28 +1,63 @@
+/*******************************************************
+ * Advent of Code - Day 1: Historian Hysteria - Part 1
+ * -----------------------------------------------------
+ * Author      : Parallaxes
+ * Date        : 12-01-2024
+ * Description : Very simple approach where we parse the
+ *               input file and store the numbers in two
+ *               separate vectors. We then sort the 
+ *               vectors and calculate the sum of the 
+ *               absolute differences.
+ * Input File  : input.txt
+ * Language    : Rust
+ * -----------------------------------------------------
+ * Notes:
+ * - N/A
+ *******************************************************/
+
 use std::fs;
 
+// Driver
 fn main() {
+    // Read the contents of the input file
     let contents = fs::read_to_string("input.txt").expect("Error");
+
+    // Parse the contents and store the two vectors
     let (v1, v2) = parse(&contents);
+
+    // Solve the problem and print the result
     println!("{}", solve(&v1, &v2));
 }
 
+// Parse function
 fn parse(contents: &str) -> (Vec<i32>, Vec<i32>) {
+    // Create two new vectors
     let mut v1 = Vec::new();
     let mut v2 = Vec::new();
+
+    // For each line in the input file...
     for line in contents.lines() {
+        // Split the line into a tuple of integers
         if let Some((first, second)) = split(line) {
             v1.push(first);
             v2.push(second);
         }
     }
 
+    // Sort the vectors
     v1.sort();
     v2.sort();
+
+    // Return the two vectors
     (v1, v2)
 }
 
+// Helper split function
 fn split(line: &str) -> Option<(i32, i32)> {
+    // Split the line into a vector of integers
     let mut nums = line.split_whitespace().map(|s| s.parse::<i32>());
+
+    // If we have two integers, return them as a tuple. Data safety!
     if let (Some(Ok(first)), Some(Ok(second))) = (nums.next(), nums.next()) {
         Some((first, second))
     } else {
@@ -30,11 +65,17 @@ fn split(line: &str) -> Option<(i32, i32)> {
     }
 }
 
+// Solve function
 fn solve(v1: &Vec<i32>, v2: &Vec<i32>) -> i32 {
+    // Initialize the sum
     let mut sum: i32 = 0;
+
+    // For each index in the vectors...
     for i in 0..v1.len() {
+        // Add the absolute difference of the two numbers to the sum
         sum += (v1[i] - v2[i]).abs();
     }
 
+    // Return the sum
     sum
 }
---
+++ 2024/day1/part2/src/main.rs
@@ -0,0 +1,72 @@
+/*******************************************************
+ * Advent of Code - Day 1: Historian Hysteria - Part 2
+ * -----------------------------------------------------
+ * Author      : Parallaxes
+ * Date        : 12-01-2024
+ * Description : Basically a simple approach where we
+ *               parse the input file and store the
+ *               frequencies of the first and second
+ *               numbers in two separate hashmaps.
+ * Input File  : input.txt
+ * Language    : Rust
+ * -----------------------------------------------------
+ * Notes:
+ * - N/A
+ *******************************************************/
+
+use std::fs;
+use std::collections::HashMap;
+
+// Driver
+fn main() {
+    // Read the contents of the input file
+    let contents = fs::read_to_string("input.txt").expect("Error");
+
+    // Parse the contents and store the two hash maps
+    let (v1, v2) = parse(&contents);
+
+    // Solve the problem and print the result
+    println!("{}", solve(&v1, &v2));
+}
+
+// Parse function
+fn parse(contents: &str) -> (HashMap<i32, i32>, HashMap<i32, i32>) {
+    // Create two new hash maps
+    let mut v1 = HashMap::new();
+    let mut v2 = HashMap::new();
+    
+    // For each line in the input file...
+    for line in contents.lines() {
+        // Split the line into a vector of integers
+        let nums: Vec<i32> = line
+            .split_whitespace()
+            .filter_map(|num| num.parse().ok())
+            .collect();
+    
+        // Increment the frequency of the first and second numbers
+        // Just assume that the data is clean and there are always two numbers
+        *v1.entry(nums[0]).or_insert(0) += 1;
+        *v2.entry(nums[1]).or_insert(0) += 1;
+    }
+
+    // Return the two hash maps
+    (v1, v2)
+}
+
+// Solve function
+fn solve(v1: &HashMap<i32, i32>, v2: &HashMap<i32, i32>) -> i32 {
+    // Initialize the sum
+    let mut sum: i32 = 0;
+
+    // For each key-value pair in the first hash map...
+    for (key, value) in v1 {
+        // If the key exists in the second hash map...
+        if let Some(second_freq) = v2.get(key) {
+            // Add the product of the key and the frequency of the second number to the sum
+            sum += key * second_freq;
+        }
+    }
+
+    // Return the sum
+    sum
+}
